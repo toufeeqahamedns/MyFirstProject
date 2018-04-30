55c686f3d019003de63bd4c9e635f779cc1d675f
Mohammed Toufeeq Ahamed <toufeeqahamedns@gmail.com>
2018-04-30 13:18:58 +0530

    Creates a ArrayList of Word object. Adds a ListView with each View having 2 TextViews to the NumbersActivity with the help of WordAdapter which extends ArrayAdapter.

---
+++ app/src/main/java/com/example/toufeeqahamed/myfirstproject/WordAdapter.java
@@ -0,0 +1,47 @@
+package com.example.toufeeqahamed.myfirstproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listView = convertView;
+        if(listView == null){
+            listView = LayoutInflater.from(getContext()).inflate(R.layout.list_view, parent,false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
+        TextView miwokTextView = listView.findViewById(R.id.miwok_text_view);
+        // Get the Miwok translation from the currentWord object and set this text on
+        // the Miwok TextView.
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID default_text_view.
+        TextView defaultTextView = listView.findViewById(R.id.default_text_view);
+        // Get the default translation from the currentWord object and set this text on
+        // the default TextView.
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listView;
+
+
+    }
+}
