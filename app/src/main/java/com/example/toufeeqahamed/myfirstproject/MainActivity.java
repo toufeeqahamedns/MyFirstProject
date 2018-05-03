f7d3abeab8b1186b1638e84f485bfc7498f31110
Mohammed Toufeeq Ahamed <toufeeqahamedns@gmail.com>
2018-05-03 19:02:23 +0530

    Visual Polish

--- app/src/main/java/com/example/toufeeqahamed/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/toufeeqahamed/myfirstproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.toufeeqahamed.myfirstproject;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,15 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_category.xml layout file
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.activity_main);
 
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
